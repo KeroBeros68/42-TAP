fc18b10a8f8c231d4194da42d424bd42bc9c9279
Gaspard TOURDIAT <gaspard.tourdiat@gmail.com>
2026-09-24 18:09:58 +0200

    feat-wip: progress tests

---
+++ test/globals.hpp
@@ -0,0 +1,7 @@
+#ifndef GLOBALS_HPP
+# define GLOBALS_HPP
+
+# define WINDOW_HEIGHT 720
+# define WINDOW_WIDTH 1080
+
+#endif
--- test/main.cpp
+++ test/main.cpp
@@ -1,18 +1,19 @@
 #include <QApplication>
 #include <QMainWindow>
 #include <QLabel>
+#include "globals.hpp"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     QMainWindow window;
-    window.setWindowTitle("Ma Fenêtre Qt");
-    window.resize(400, 300);
+    window.setWindowTitle("42TAP RFC GUI");
+    window.resize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    while (true)
+    {
+        printf("%d\n")
+    }
     
-    QLabel *label = new QLabel("Bonjour depuis Qt !", &window);
-    QLabel *label2 = new QLabel("Bonjour depuis Qttttttttttttt !", &window);
-    window.setCentralWidget(label);
-    window.setCentralWidget(label2);
 
     window.show();
     return app.exec();
